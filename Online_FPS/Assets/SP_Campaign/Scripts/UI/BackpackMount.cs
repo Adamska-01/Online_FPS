190aa64d13f87582a3f652ced934d1edfbea901f
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2023-08-07 20:45:58 +0100

    Fixed Scroll event not firing when using Event Trigger on other objects

--- Online_FPS/Assets/SP_Campaign/Scripts/UI/BackpackMount.cs
+++ Online_FPS/Assets/SP_Campaign/Scripts/UI/BackpackMount.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
 
-public class BackpackMount : MonoBehaviour
+public class BackpackMount : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [Header("Inventory Class")]
+    [SerializeField] private PlayerInventoryUI inventoryUI;
+    
     //Inspector-Assigned
+    [Header("Mount Components")]
     [SerializeField] private Image backpackMountImage = null;
     [SerializeField] private TMP_Text backpackMountEmptyText = null;
 
 
+
     //Properties
     public Image BackpackItemImage { get { return backpackMountImage; } }
-    public TMP_Text EmptyText { get { return backpackMountEmptyText; } } 
+    public TMP_Text EmptyText { get { return backpackMountEmptyText; } }
+
+
+    //Events
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        inventoryUI.OnClickBackpackMount(GetComponent<Image>());
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        inventoryUI.OnEnterBackpackMount(GetComponent<Image>());
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        inventoryUI.OnExitBackpackMount(GetComponent<Image>());
+    }
 }
