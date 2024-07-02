c979fcce2ed3f0a726418ad13e353c7f3d648fd0
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-07-02 21:46:05 +0100

    Added separate classes to handle input

---
+++ Online_FPS/Assets/SP_Campaign/Input/PlayerInput_UI.cs
@@ -0,0 +1,35 @@
+using FPS.Utility;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerInput_UI : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInventoryUI inventoryUI;
+
+
+    private PlayerInput playerInput;
+
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+		Guard.AgainstNull(playerInput, nameof(playerInput));
+		Guard.AgainstNull(inventoryUI, nameof(inventoryUI));
+	}
+
+
+    public void Toggle_Inventory_PlayerInput(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+            return;
+
+        var InventoryUIVisibility = inventoryUI.ToggleInventory();
+
+        var currentActionMap = InventoryUIVisibility ? InputActionMaps.UI_ACTION_MAP : InputActionMaps.GAMEPLAY_ACTION_MAP;
+
+        playerInput.SwitchCurrentActionMap(currentActionMap);
+	}
+}
