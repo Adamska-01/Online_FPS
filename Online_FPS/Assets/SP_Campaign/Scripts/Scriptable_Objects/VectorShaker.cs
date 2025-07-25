612f456fff27fe65cfb80649f80a099fb749f35b
Adamska-01 <silverworld@hotmail.it>
2025-07-25 12:01:56 +0200

    Fixed 'VectorShaker' (Reset to zero when shake ends)

--- Online_FPS/Assets/SP_Campaign/Scripts/Scriptable_Objects/VectorShaker.cs
+++ Online_FPS/Assets/SP_Campaign/Scripts/Scriptable_Objects/VectorShaker.cs
@@ -1,7 +1,4 @@
-using RootMotion.FinalIK;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -12,51 +9,53 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable OBJ/Vector Shaker", fileName = "New Vector Shaker")]
 public class VectorShaker : ScriptableObject
 {
-    // Inspector-Assigned
-    [Tooltip("The vector we wish to shake")]
-    [SerializeField] private SharedVector3 shakeVector = null;
+	// Inspector-Assigned
+	[Tooltip("The vector we wish to shake")]
+	[SerializeField] private SharedVector3 shakeVector = null;
 
-    // Internals 
-    protected IEnumerator coroutine = null;
+	// Internals 
+	protected IEnumerator coroutine = null;
 
 
-    public void ShakeVector(float _duration, float _magnitude, float _damping = 1.0f)
-    {
-        if (_duration < 0.001f || _magnitude.Equals(0.0f))
-            return;
+	public void ShakeVector(float _duration, float _magnitude, float _damping = 1.0f)
+	{
+		if (_duration < 0.001f || _magnitude.Equals(0.0f))
+			return;
 
-        if (shakeVector != null && SO_CoroutineRunner.Instance != null)
-        {
-            if(coroutine != null)
-            {
-                SO_CoroutineRunner.Instance.StopCoroutine(coroutine);
-            }
+		if (shakeVector != null && SO_CoroutineRunner.Instance != null)
+		{
+			if(coroutine != null)
+			{
+				SO_CoroutineRunner.Instance.StopCoroutine(coroutine);
+			}
 
-            coroutine = Shake(_duration, _magnitude, _damping);
+			coroutine = Shake(_duration, _magnitude, _damping);
 
-            SO_CoroutineRunner.Instance.StartCoroutine(coroutine);
-        }
-    }
+			SO_CoroutineRunner.Instance.StartCoroutine(coroutine);
+		}
+	}
 
-    protected IEnumerator Shake(float _duration, float _magnitude, float _damping = 1.0f)
-    {
-        float time = 0.0f;
+	protected IEnumerator Shake(float _duration, float _magnitude, float _damping = 1.0f)
+	{
+		float time = 0.0f;
 
-        while (time <= _duration)
-        {
-            float x = Random.Range(-1.0f, 1.0f) * _magnitude;
-            float y = Random.Range(-1.0f, 1.0f) * _magnitude;
+		while (time <= _duration)
+		{
+			float x = Random.Range(-1.0f, 1.0f) * _magnitude;
+			float y = Random.Range(-1.0f, 1.0f) * _magnitude;
 
-            Vector3 unSmoothedVector = new Vector3(x, y, 0.0f);
+			Vector3 unSmoothedVector = new Vector3(x, y, 0.0f);
 
-            if(shakeVector != null)
-            {
-                shakeVector.Value = Vector3.Lerp(unSmoothedVector, Vector3.zero, (time / _duration) * _damping);
-            }
+			if(shakeVector != null)
+			{
+				shakeVector.Value = Vector3.Lerp(unSmoothedVector, Vector3.zero, (time / _duration) * _damping);
+			}
 
-            time += Time.deltaTime;
+			time += Time.deltaTime;
 
-            yield return null;
-        }
-    }
+			yield return null;
+		}
+
+		shakeVector.Value = Vector3.zero;
+	}
 }
