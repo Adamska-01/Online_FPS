2246d8d7433686f12499890ba27147d2f9ddd7ed
Adamska-01 <silverworld@hotmail.it>
2025-07-25 13:55:11 +0200

    Added base call in 'ShotgunAnimatorStateCallback.OnAction'

--- Online_FPS/Assets/SP_Campaign/Scripts/FPS_Controller/Animator_StateMachine_Callbacks/ShotgunAnimatorStateCallback.cs
+++ Online_FPS/Assets/SP_Campaign/Scripts/FPS_Controller/Animator_StateMachine_Callbacks/ShotgunAnimatorStateCallback.cs
@@ -1,25 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 public class ShotgunAnimatorStateCallback : WeaponAnimatorStateCallback
 {
-    [SerializeField] GameObject shotgunShell = null;
+	[SerializeField] 
+	GameObject shotgunShell = null;
 
-    public override void OnAction(string _context, CharacterManager _chrMgr = null)
-    {
-        if (shotgunShell == null)
-            return;
 
-        switch(_context)
-        {
-            case "Disable Shotgun Shell":
-                shotgunShell.SetActive(false);
-                break;
-            case "Enable Shotgun Shell":
-                shotgunShell.SetActive(true);
-                break;
-        }
-    }
+	public override void OnAction(string _context, CharacterManager _chrMgr = null)
+	{
+		base.OnAction(_context, _chrMgr);
+		
+		if (shotgunShell == null)
+			return;
+
+		switch(_context)
+		{
+			case "Disable Shotgun Shell":
+				shotgunShell.SetActive(false);
+				break;
+			case "Enable Shotgun Shell":
+				shotgunShell.SetActive(true);
+				break;
+		}
+	}
 }
